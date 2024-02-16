2540fd6a37dc96915533e9396281ebad66775dff
ArpanSankesh <arpansankesh830@gmail.com>
2024-02-16 15:20:02 +0530

    section-2

---
+++ script.js
@@ -0,0 +1,10 @@
+// var tl = gsap.timeline('.hero-slider',{
+//     scrollTrigger:{
+//         Trigger:'.hero-slider',
+//         start:'50% 50%',
+//         end:'50% 50%',
+//         marker:true
+//     }
+// })
+
+
