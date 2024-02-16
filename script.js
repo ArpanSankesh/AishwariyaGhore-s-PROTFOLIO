0e06bea13fdc6dd3b55b63b396f40763c576f534
ArpanSankesh <arpansankesh830@gmail.com>
2024-02-16 18:25:29 +0530

    about section

--- script.js
+++ script.js
@@ -25,20 +25,75 @@ gsap.to('.hero h1:nth-child(3)',{
     delay:0.5,
     opacity:1,
     translateX:0
+
 })
 
 var tl = gsap.timeline({
     scrollTrigger:{
         trigger:'.section-2',
-        start:'-20% 50%',
+        start:'-50% 50%',
         end:'50% 50%',
-        markers:true,
+        scrub:2
+    }
+})
+tl.to('.hero h1:nth-child(1)',{
+    opacity:1,
+    translateX:"-50vw",
+},"a")
+tl.to('.hero h1:nth-child(2)',{
+    opacity:1,
+    translateX:"50vw"
+},"a")
+tl.to('.hero h1:nth-child(3)',{
+    opacity:1,
+    translateX:"-50vw"
+},"a")
+
+
+var tl1 = gsap.timeline({
+    scrollTrigger:{
+        trigger:'.section-2',
+        start:'-20% 50%',
+        end:'100% 50%',
         scrub:true
     }
 })
 
-tl.to('.section-2',{
-    borderTopLeftRadius:0,
-    borderTopRightRadius:0
+tl1.to('.section-2',{
+    // borderTopLeftRadius:"1%",
+    // borderTopRightRadius:"1%"
+    borderRadius:0
 })
 
+
+var tl2 = gsap.timeline({
+    scrollTrigger:{
+        trigger:'.text-area',
+        start:'0% 50%',
+        end:'100% 50%',
+        scrub:2,
+        // pin:true
+    }
+})
+
+tl.to('.text-area-hover',{
+    width:'100%'
+})
+var tl3 = gsap.timeline({
+    scrollTrigger:{
+        trigger:'.section-4',
+        start:'0% 50%',
+        end:'100% 50%',
+        scrub:2
+    }
+})
+
+tl.to('.right img',{
+    borderRadius:"10%",
+    opacity:1,
+    marginTop:0, 
+},"b")
+tl.to('.left',{
+    marginTop:0,
+    opacity:1
+},"b")
