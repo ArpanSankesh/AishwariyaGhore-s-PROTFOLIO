0b82eebe3dd1ef8b15cf543e334c302d4a33199a
ArpanSankesh <arpansankesh830@gmail.com>
2024-02-16 15:41:27 +0530

    hero animation

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
-// var tl = gsap.timeline('.hero-slider',{
-//     scrollTrigger:{
-//         Trigger:'.hero-slider',
-//         start:'50% 50%',
-//         end:'50% 50%',
-//         marker:true
-//     }
-// })
+var tl = gsap.timeline('.section-2',{
+    scrollTrigger:{
+        trigger:'.section-2',
+        start:'50% 50%',
+        end:'50% 50%',
+        markers:true
+    }
+})
+
+gsap.to('.hero h1:nth-child(1)',{
+    delay:0.5,
+    opacity:1,
+    translateX:0
+})
+gsap.to('.hero h1:nth-child(2)',{
+    delay:0.5,
+    opacity:1,
+    translateX:0
+})
+gsap.to('.hero h1:nth-child(3)',{
+    delay:0.5,
+    opacity:1,
+    translateX:0
+})
 
 
