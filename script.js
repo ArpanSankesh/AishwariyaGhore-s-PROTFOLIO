23ba4517e08e93b8da50fa8a1e90b50d19cffffc
ArpanSankesh <arpansankesh830@gmail.com>
2024-02-17 11:55:31 +0530

    creates hammenu

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
-// var tl = gsap.timeline({
-//     scrollTrigger:{
-//         trigger:'.section-2',
-//         scroller:'body',
-//         start:'50% 50%',
-//         end:'50% 50%',
-//         markers:true
-//     }
-// })
+var menuBtn = document.querySelector('.menu-btn i');
+var hamMenu = document.querySelector('.ham-menu');
+var hamItems = document.querySelector('.menu-wraper');
+var logo = document.querySelector('.portfolio-txt h1');
+let flag = 0;
+var dropDownMenu = function(){
+    if (flag === 0){
+        hamMenu.style.top = "0";
+        hamMenu.style.right = "0";
+        hamMenu.style.borderRadius = "0";
+        logo.style.color = "#fff";
+        hamItems.style.opacity = 1;
+        menuBtn.style.color = "#fff";
+        flag = 1;
+    }else{
+        hamMenu.style.top = "-100%";
+        hamMenu.style.right = "-100%";
+        hamMenu.style.borderRadius = "50%";
+        hamItems.style.opacity = 0;
+        logo.style.color = "#0c0c0c";
+        menuBtn.style.color = "#0c0c0c";
+        flag = 0;
+    }
+}
+
+menuBtn.addEventListener('click',dropDownMenu)
+
+
 gsap.from('.nav-bar',{
-    y:"-300px"
+    y:"-300px",
+    // stagger: 0.1
 })
 
 gsap.to('.hero h1:nth-child(1)',{
@@ -50,20 +70,7 @@ tl.to('.hero h1:nth-child(3)',{
 },"a")
 
 
-var tl1 = gsap.timeline({
-    scrollTrigger:{
-        trigger:'.section-2',
-        start:'-20% 50%',
-        end:'100% 50%',
-        scrub:true
-    }
-})
 
-tl1.to('.section-2',{
-    // borderTopLeftRadius:"1%",
-    // borderTopRightRadius:"1%"
-    borderRadius:0
-})
 
 
 var tl2 = gsap.timeline({
@@ -76,9 +83,10 @@ var tl2 = gsap.timeline({
     }
 })
 
-tl.to('.text-area-hover',{
+tl2.to('.text-area-hover',{
     width:'100%'
-})
+}) 
+
 var tl3 = gsap.timeline({
     scrollTrigger:{
         trigger:'.section-4',
@@ -97,3 +105,16 @@ tl.to('.left',{
     marginTop:0,
     opacity:1
 },"b")
+
+var tl4 = gsap.timeline({
+    scrollTrigger:{
+        trigger:'.text-area2',
+        start:'0% 50%',
+        end:'100% 50%',
+        scrub:2,
+    }
+})
+
+tl4.to('.text-area-hover2',{
+    width:'100%'
+}) 
